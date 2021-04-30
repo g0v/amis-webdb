5891d71898f72cf721c2151f8b7dfec45ce95f62
wildjcrt <wildjcrt@yahoo.com.tw>
2021-04-30 10:30:59 +0800

    Setup rails_admin config

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,12 +1,13 @@
 RailsAdmin.config do |config|
+  config.main_app_name = ['蔡中涵大辭典', "資料庫"]
 
   ### Popular gems integration
 
   ## == Devise ==
-  # config.authenticate_with do
-  #   warden.authenticate! scope: :user
-  # end
-  # config.current_user_method(&:current_user)
+  config.authenticate_with do
+    warden.authenticate! scope: :user
+  end
+  config.current_user_method(&:current_user)
 
   ## == CancanCan ==
   # config.authorize_with :cancancan
@@ -15,7 +16,7 @@ RailsAdmin.config do |config|
   # config.authorize_with :pundit
 
   ## == PaperTrail ==
-  # config.audit_with :paper_trail, 'User', 'PaperTrail::Version' # PaperTrail >= 3.0.0
+  config.audit_with :paper_trail, 'User', 'PaperTrail::Version' # PaperTrail >= 3.0.0
 
   ### More at https://github.com/sferik/rails_admin/wiki/Base-configuration
 
